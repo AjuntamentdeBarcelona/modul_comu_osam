bb64925f42c0aea521376a06a0885b69ee5736b8
Sergio Casero <sergio.casero@worldline.com>
2020-05-26 15:59:31 +0200

    Added iOS

---
+++ ios/commonmodule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  commonmodule
+//
+//  Created by Sergio Casero Hernández on 26/05/2020.
+//  Copyright © 2020 Sergio Casero Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
