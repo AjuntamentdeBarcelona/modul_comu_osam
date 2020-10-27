1baf98ab375ea350df0253330ad6e7ab01bc095e
Sergio <sergio.casero23@gmail.com>
2020-10-27 14:55:04 +0100

    Implemented the option to dismiss the dialog when comparison is LAZY or INFO!

--- common/src/iosMain/kotlin/cat/bcn/commonmodule/ui/versioncontrol/VersionControl.kt
+++ common/src/iosMain/kotlin/cat/bcn/commonmodule/ui/versioncontrol/VersionControl.kt
@@ -2,15 +2,15 @@ package cat.bcn.commonmodule.ui.versioncontrol
 
 import cat.bcn.commonmodule.data.datasource.remote.CommonRemote
 import cat.bcn.commonmodule.model.App
+import cat.bcn.commonmodule.model.Version
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import platform.UIKit.UIAlertAction
-import platform.UIKit.UIAlertActionStyleDefault
-import platform.UIKit.UIAlertController
-import platform.UIKit.UIViewController
+import platform.UIKit.*
 
-actual class VersionControl constructor(val vc: UIViewController) {
+actual class VersionControl constructor(
+    private val vc: UIViewController
+) {
     actual fun check(
         appId: String,
         versionCode: Int,
@@ -38,7 +38,21 @@ actual class VersionControl constructor(val vc: UIViewController) {
                 )
             )
 
+            when (version.comparisonMode) {
+                Version.ComparisonMode.FORCE -> {
+                    // Do nothing, by default will be not dismissable
+                }
+                else -> {
+                    alert.addAction(UIAlertAction.actionWithTitle(
+                        title = version.cancel(language),
+                        style = UIAlertActionStyleCancel,
+                        handler = { f(VersionControlResponse.CANCELLED) }
+                    ))
+                }
+            }
+
             vc.presentViewController(alert, animated = true, completion = null)
         }
     }
+
 }
--- ios/commonmodule/ViewController.swift
+++ ios/commonmodule/ViewController.swift
@@ -29,6 +29,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-
 }
 
