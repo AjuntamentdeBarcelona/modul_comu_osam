07ac7c5e28afce9fd9a51b9f0995ae78c486d935
Sergio <sergio.casero23@gmail.com>
2020-10-27 16:29:47 +0100

    Displaying toast

--- android/src/main/java/presentation/ui/view/activity/HomeActivity.kt
+++ android/src/main/java/presentation/ui/view/activity/HomeActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import cat.bcn.commonmodule.ui.versioncontrol.Language
 import cat.bcn.commonmodule.ui.versioncontrol.OSAM
-import cat.bcn.commonmodule.ui.versioncontrol.VersionControlResponse
 import com.app.app.R
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -23,16 +22,7 @@ class HomeActivity : AppCompatActivity() {
                 versionCode = 20141104,
                 language = Language.CA
             ) {
-                Toast.makeText(
-                    this@HomeActivity,
-                    when (it) {
-                        VersionControlResponse.ACCEPTED -> "Dialog ACCEPTED"
-                        VersionControlResponse.DISMISSED -> "Dialog DISMISSED"
-                        VersionControlResponse.CANCELLED -> "Dialog CANCELLED"
-                        VersionControlResponse.ERROR -> "Dialog ERROR"
-                    },
-                    Toast.LENGTH_LONG
-                ).show()
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
             }
         }
 
