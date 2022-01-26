44682a7d2f9dc164f3d9a435627c54efe659b13c
Jacobo Conejo Lima <jacobo.conejolima@worldline.com>
2022-01-26 15:50:29 +0100

    Added tasks to generate xcframeworks and publish them (iOS)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,16 +1,21 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+import java.text.SimpleDateFormat
+import java.util.*
 
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
     id("com.android.library")
     id("maven-publish")
-    kotlin("native.cocoapods")
 }
 
 val libName = "OSAMCommon"
-group = "com.github.AjuntamentdeBarcelona"
-version = "1.0.5"
+val libGroup = "com.github.AjuntamentdeBarcelona"
+val libVersionName = "1.0.6"
+group = libGroup
+version = libVersionName
+
+val xcframeworkDestinationRepo = "common_module_mobile_frameworks"
 
 kotlin {
     android()
@@ -73,25 +78,134 @@ kotlin {
         }
     }
 
-    cocoapods {
-        summary = "Common library for the osam version and rating app control"
-        homepage = "https://github.com/AjuntamentdeBarcelona/modul_comu_osam"
-        framework {
-            baseName = "OSAMCommon"
-        }
-        license = "BSD"
-        authors = "Eduard Carbonell eduard.carbonell@worldline.com"
-        ios.deploymentTarget = "13.0"
-        //podfile = project.file("../ios/Podfile")
-    }
+    tasks {
 
-    val podspec = tasks["podspec"] as org.jetbrains.kotlin.gradle.tasks.PodspecTask
-    podspec.doLast {
-        val podspec = file("${project.name.replace("-", "_")}.podspec")
-        val newPodspecContent = podspec.readLines().map {
-            if (it.contains("spec.source")) "    spec.source                   = { :git => \"https://github.com/AjuntamentdeBarcelona/modul_comu_osam.git\", :tag => \"$version\" }" else it
+        register("publishDevFramework") {
+            description = "Publish iOs framework to the Cocoa Repo"
+
+            project.exec {
+                workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                commandLine("git", "checkout", "develop").standardOutput
+            }
+
+            dependsOn("assemble${libName}DebugXCFramework")
+
+            doLast {
+
+                copy {
+                    from("$buildDir/XCFrameworks/debug")
+                    into("$rootDir/../$xcframeworkDestinationRepo")
+                }
+
+                val dir = File("$rootDir/../$xcframeworkDestinationRepo/$libName.podspec")
+                val tempFile = File("$rootDir/../$xcframeworkDestinationRepo/$libName.podspec.new")
+
+                val reader = dir.bufferedReader()
+                val writer = tempFile.bufferedWriter()
+                var currentLine: String?
+
+                while (reader.readLine().also { currLine -> currentLine = currLine } != null) {
+                    if (currentLine?.startsWith("s.version") == true) {
+                        writer.write("s.version       = \"${libVersionName}\"" + System.lineSeparator())
+                    } else {
+                        writer.write(currentLine + System.lineSeparator())
+                    }
+                }
+                writer.close()
+                reader.close()
+                val successful = tempFile.renameTo(dir)
+
+                if (successful) {
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine(
+                            "git",
+                            "add",
+                            "."
+                        ).standardOutput
+                    }
+
+                    val dateFormatter = SimpleDateFormat("dd/MM/yyyy - HH:mm", Locale.getDefault())
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine(
+                            "git",
+                            "commit",
+                            "-m",
+                            "\"New dev release: ${libVersionName}-${dateFormatter.format(Date())}\""
+                        ).standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine("git", "push", "origin", "develop").standardOutput
+                    }
+                }
+            }
         }
-        podspec.writeText(newPodspecContent.joinToString(separator = "\n"))
+
+        register("publishFramework") {
+            description = "Publish iOs framework to the Cocoa Repo"
+
+            project.exec {
+                workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                commandLine("git", "checkout", "master").standardOutput
+            }
+
+            // Create Release Framework for Xcode
+            dependsOn("assemble${libName}ReleaseXCFramework")
+
+            // Replace
+            doLast {
+
+                copy {
+                    from("$buildDir/XCFrameworks/release")
+                    into("$rootDir/../$xcframeworkDestinationRepo")
+                }
+
+                val dir = File("$rootDir/../$xcframeworkDestinationRepo/$libName.podspec")
+                val tempFile = File("$rootDir/../$xcframeworkDestinationRepo/$libName.podspec.new")
+
+                val reader = dir.bufferedReader()
+                val writer = tempFile.bufferedWriter()
+                var currentLine: String?
+
+                while (reader.readLine().also { currLine -> currentLine = currLine } != null) {
+                    if (currentLine?.startsWith("s.version") == true) {
+                        writer.write("s.version       = \"${libVersionName}\"" + System.lineSeparator())
+                    } else {
+                        writer.write(currentLine + System.lineSeparator())
+                    }
+                }
+                writer.close()
+                reader.close()
+                val successful = tempFile.renameTo(dir)
+
+                if (successful) {
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine("git", "add", ".").standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine("git", "commit", "-m", "\"New release: ${libVersionName}\"").standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine("git", "tag", libVersionName).standardOutput
+                    }
+
+                    project.exec {
+                        workingDir = File("$rootDir/../$xcframeworkDestinationRepo")
+                        commandLine("git", "push", "origin", "master", "--tags").standardOutput
+                    }
+                }
+            }
+        }
     }
 }
 
