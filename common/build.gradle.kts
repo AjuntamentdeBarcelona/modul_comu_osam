ad74d691f5fd0870d3c1107ca02da1f073124cf0
Jacobo Conejo Lima <jacobo.conejolima@worldline.com>
2022-02-22 11:35:09 +0100

    Updated version

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 val libName = "OSAMCommon"
 val libGroup = "com.github.AjuntamentdeBarcelona"
-val libVersionName = "1.0.10"
+val libVersionName = "1.0.11"
 group = libGroup
 version = libVersionName
 
