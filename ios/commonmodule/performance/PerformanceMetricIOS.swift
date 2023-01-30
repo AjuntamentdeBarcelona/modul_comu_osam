b012eed5b24ca3e37a9862286dc114c24d2704ac
Xavier Febrer Jordà <xavier.febrer@worldline.com>
2023-01-30 15:09:44 +0100

    Add logs.

--- ios/commonmodule/performance/PerformanceMetricIOS.swift
+++ ios/commonmodule/performance/PerformanceMetricIOS.swift
@@ -15,7 +15,7 @@ class PerformanceMetricIOS: PerformanceMetric {
         metric.start()
     }
     func setRequestPayloadSize(bytes: Int64) {
-        print("PerformanceMetricIOS setRequestPayloadSize")
+        print("PerformanceMetricIOS setRequestPayloadSize bytes: \(bytes)")
         metric.requestPayloadSize = Int(bytes)
     }
     func markRequestComplete() {
@@ -27,19 +27,19 @@ class PerformanceMetricIOS: PerformanceMetric {
         // not used for iOs
     }
     func setResponseContentType(contentType: String) {
-        print("PerformanceMetricIOS setResponseContentType")
+        print("PerformanceMetricIOS setResponseContentType contentType: \(contentType)")
         metric.responseContentType = contentType
     }
     func setHttpResponseCode(responseCode: Int32) {
-        print("PerformanceMetricIOS setHttpResponseCode")
+        print("PerformanceMetricIOS setHttpResponseCode responseCode: \(responseCode)")
         metric.responseCode = Int(responseCode)
     }
     func setResponsePayloadSize(bytes: Int64) {
-        print("PerformanceMetricIOS setResponsePayloadSize")
+        print("PerformanceMetricIOS setResponsePayloadSize bytes: \(bytes)")
         metric.responsePayloadSize = Int(bytes)
     }
     func putAttribute(attribute: String, value: String) {
-        print("PerformanceMetricIOS putAttribute")
+        print("PerformanceMetricIOS putAttribute attribute: \(attribute), value: \(value)")
         metric.setValue(value, forAttribute: attribute)
     }
     func stop() {
