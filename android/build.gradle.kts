03221655ca357a7297571d293ed595700f8336ea
Sergio Casero Hernández <sergio.casero@worldline.com>
2024-10-23 12:17:00 +0200

    Project sync is working

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,16 +6,16 @@ import com.android.build.gradle.internal.dsl.ProductFlavor
 import com.android.build.gradle.internal.dsl.SigningConfig
 
 plugins {
-    id("com.android.application")
-
-    kotlin("android")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
-    id("com.google.firebase.firebase-perf")
+    alias(libs.plugins.androidApp)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.googleServices)
+    alias(libs.plugins.firebaseCrashlytics)
+    alias(libs.plugins.firebasePerformance)
 }
 
 android {
     compileSdk = App.targetSdkVersion
+    namespace = App.applicationId
 
     defaultConfig {
         minSdk = App.minSdkVersion
@@ -36,17 +36,18 @@ android {
     applicationVariants.all { applyProperties() }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     lint {
         abortOnError = false
     }
+
     buildFeatures {
         viewBinding = true
     }
@@ -57,21 +58,20 @@ dependencies {
 
     implementation(project(":common"))
 
-    implementation(Dependencies.Android.appCompat)
-    implementation(Dependencies.Android.constraintLayout)
-    implementation(Dependencies.Android.recycler)
+    implementation(libs.appCompat)
+    implementation(libs.constraintLayout)
+    implementation(libs.recycler)
 
-    implementation(Dependencies.Android.multidex)
+    implementation(libs.multidex)
 
-    implementation(platform(Dependencies.Android.firebaseBoM))
-    implementation(Dependencies.Android.analytics)
-    implementation(Dependencies.Android.firebasePerf)
-    implementation(Dependencies.Android.firebaseCrashlytics)
+    implementation(libs.analytics)
+    implementation(libs.firebasePerf)
+    implementation(libs.firebaseCrashlytics)
 
-    implementation(Dependencies.Android.coroutinesAndroid)
-    implementation(Dependencies.Android.coroutinesPlayServices)
+    implementation(libs.coroutinesAndroid)
+    implementation(libs.coroutinesPlayServices)
 
-    androidTestImplementation(Dependencies.Android.Test.runner)
+    androidTestImplementation(libs.junitRunner)
 }
 
 fun NamedDomainObjectContainer<ApplicationProductFlavor>.createProductFlavor(appFlavor: AppFlavor) =
