fd19d3d4c96936db4403c3d8ec040295d182475e
Gustavo Lorena <osamgit@gmail.com>
2026-02-26 12:52:56 +0100

    Update to version 3.0.14-dev

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -16,7 +16,7 @@ allOpen {
 
 val libName = "OSAMCommon"
 val libGroup = "com.github.AjuntamentdeBarcelona"
-val libVersionName = "3.0.13-dev"
+val libVersionName = "3.0.14-dev"
 
 group = libGroup
 version = libVersionName
