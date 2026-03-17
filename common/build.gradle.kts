67782bc53f23359cdc4cba1be5e0278b2fb963df
Alberto Aguilar <alberto.aguilar@worldline.com>
2026-03-17 15:10:50 +0100

    Update files to version 3.1.2-dev

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -16,7 +16,7 @@ allOpen {
 
 val libName = "OSAMCommon"
 val libGroup = "com.github.AjuntamentdeBarcelona"
-val libVersionName = "3.1.1-dev"
+val libVersionName = "3.1.2-dev"
 
 group = libGroup
 version = libVersionName
